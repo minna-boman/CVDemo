d75456b1f3fc1255ffe1fa794c133835b9e5db2b
Minna Boman <minna.boman@gmail.com>
2020-09-28 16:55:16 +0300

    Ensimmäiset versiot tuotu versionhallintaan

---
+++ javascript.js
@@ -0,0 +1,19 @@
+function myFunction2(){
+    var x =document.getElementById("myDiv1");
+    if (x.style.display==="none"){
+        x.style.display="block";
+ 
+    } else {
+        x.style.display="none";
+    }
+}
+
+function myFunction(){
+    var x =document.getElementById("myDiv2");
+    if (x.style.display==="none"){
+        x.style.display="block";
+ 
+    } else {
+        x.style.display="none";
+    }
+}
